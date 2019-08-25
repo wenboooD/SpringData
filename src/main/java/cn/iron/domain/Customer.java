14040a47c03560949d382fd8594aa3e05f67edca
ironman <981907700@qq.com>
2019-08-25 20:17:54 +0800

    final commit on 2019/8/25 20:17

--- src/main/java/cn/iron/domain/Customer.java
+++ src/main/java/cn/iron/domain/Customer.java
@@ -1,4 +1,121 @@
 package cn.iron.domain;
 
+import javax.persistence.*;
+/**
+ * 客户的实体类
+ *      配置映射关系
+ *
+ *   1.实体类和表的映射关系
+ *      @Entity:声明实体类
+ *      @Table : 配置实体类和表的映射关系
+ *          name : 配置数据库表的名称
+ *   2.实体类中属性和表中字段的映射关系
+ */
+@Entity//生命实体类
+@Table(name = "cst_customer")
 public class Customer {
+
+    /**
+     * @Id:声明主键的配置
+     * @GeneratedValue:配置主键的生成策略 JPA规范提供的四种主键生成策略
+     *      strategy:
+     *          1.GenerationType.IDENTITY : 自增 mysql
+     *          2.GenerationType.SEQUENCE : 序列 oracle
+     *          3.GenerationType.TABLE : jpa提供的一种机制，通过一张数据库表的形式帮助我们完成主键自增
+     *          4.GenerationType.AUTO ： 由程序自动的帮助我们选择主键生成策略
+     *
+     * @Column:配置属性和字段的映射关系
+     *      name：数据库表中字段的名称
+     * */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cust_id")
+    private Long custId;
+
+    @Column(name = "cust_name")
+    private String custName;
+
+    @Column(name = "cust_source")
+    private String custSource;
+
+    @Column(name = "cust_industry")
+    private String custIndustry;
+
+    @Column(name = "cust_level")
+    private String custLevel;
+
+    @Column(name = "cust_address")
+    private String custAddress;
+
+    @Column(name = "cust_phone")
+    private String custPhone;
+
+    public Long getCustId() {
+        return custId;
+    }
+
+    public void setCustId(Long custId) {
+        this.custId = custId;
+    }
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public String getCustSource() {
+        return custSource;
+    }
+
+    public void setCustSource(String custSource) {
+        this.custSource = custSource;
+    }
+
+    public String getCustIndustry() {
+        return custIndustry;
+    }
+
+    public void setCustIndustry(String custIndustry) {
+        this.custIndustry = custIndustry;
+    }
+
+    public String getCustLevel() {
+        return custLevel;
+    }
+
+    public void setCustLevel(String custLevel) {
+        this.custLevel = custLevel;
+    }
+
+    public String getCustAddress() {
+        return custAddress;
+    }
+
+    public void setCustAddress(String custAddress) {
+        this.custAddress = custAddress;
+    }
+
+    public String getCustPhone() {
+        return custPhone;
+    }
+
+    public void setCustPhone(String custPhone) {
+        this.custPhone = custPhone;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "custId=" + custId +
+                ", custName='" + custName + '\'' +
+                ", custSource='" + custSource + '\'' +
+                ", custIndustry='" + custIndustry + '\'' +
+                ", custLevel='" + custLevel + '\'' +
+                ", custAddress='" + custAddress + '\'' +
+                ", custPhone='" + custPhone + '\'' +
+                '}';
+    }
 }
---
+++ src/test/java/cn/iron/test/JpaTest.java
@@ -0,0 +1,126 @@
+package cn.iron.test;
+
+import cn.iron.domain.Customer;
+import cn.iron.utils.JpaUtils;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class JpaTest {
+    /**
+     * 测试jpa的保存
+     *      案例：保存一个客户到数据库中
+     *  Jpa的操作步骤
+     *     1.加载配置文件创建工厂（实体管理器工厂）对象
+     *     2.通过实体管理器工厂获取实体管理器
+     *     3.获取事务对象，开启事务
+     *     4.完成增删改查操作
+     *     5.提交事务（回滚事务）
+     *     6.释放资源
+     */
+    @Test
+    public void testSave(){
+        //调用JpaUtils中的getEntityManager方法获取实体管理器
+        EntityManager em = JpaUtils.getEntityManager();
+        //获取事务对象,开启事务
+        EntityTransaction et = em.getTransaction();
+        //开启绿色心情
+        et.begin();
+        //保存操作
+        Customer customer = new Customer();
+        customer.setCustName("zhangsan");
+        customer.setCustIndustry("it");
+        em.persist(customer);
+        //提交事务
+        et.commit();
+        //释放资源
+        em.close();
+        JpaUtils.closed();
+    }
+
+    /**
+     * 根据id查询客户
+     *  使用find方法查询：
+     *      1.查询的对象就是当前客户对象本身
+     *      2.在调用find方法的时候，就会发送sql语句查询数据库
+     *
+     *  立即加载
+     */
+    @Test
+    public void testFind(){
+        EntityManager em = JpaUtils.getEntityManager();
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        /**
+         * find : 根据id查询数据
+         *      class：查询数据的结果需要包装的实体类类型的字节码
+         *      id：查询的主键的取值
+         */
+        Customer customer = em.find(Customer.class,1l);
+        et.commit();
+        em.close();
+    }
+
+    /**
+     * 根据id查询客户
+     *      getReference方法
+     *          1.获取的对象是一个动态代理对象
+     *          2.调用getReference方法不会立即发送sql语句查询数据库
+     *              * 当调用查询结果对象的时候，才会发送查询的sql语句：什么时候用，什么时候发送sql语句查询数据库
+     *
+     * 延迟加载（懒加载） -- 提高程序的性能！！
+     *      * 得到的是一个动态代理对象 对Customer对象（原生的Customer对象属性是没有任何值）的代理
+     *      * 什么时候用（获取属性值），什么使用才会查询。
+     */
+    @Test
+    public  void testReference() {
+        //1.通过工具类获取entityManager
+        EntityManager entityManager = JpaUtils.getEntityManager();
+        //2.开启事务
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        //3.增删改查 -- 根据id查询客户
+        /**
+         * getReference : 根据id查询数据
+         *      class：查询数据的结果需要包装的实体类类型的字节码
+         *      id：查询的主键的取值
+         */
+        Customer customer = entityManager.getReference(Customer.class, 1l);
+        System.out.print(customer);
+        //4.提交事务
+        tx.commit();
+        //5.释放资源
+        entityManager.close();
+    }
+
+    /**
+     * 删除客户的案例
+     */
+    @Test
+    public void testRemove(){
+        EntityManager entityManager = JpaUtils.getEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        Customer customer = entityManager.find(Customer.class,2l);
+        entityManager.remove(customer);
+        transaction.commit();
+        entityManager.close();
+    }
+
+    /**
+     * 更新客户的操作
+     *      merge(Object)
+     */
+    @Test
+    public void testUpdate(){
+        EntityManager entityManager = JpaUtils.getEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        Customer customer = entityManager.find(Customer.class, 1l);
+        customer.setCustName("zhangsan");
+        entityManager.merge(customer);
+        transaction.commit();
+        entityManager.close();
+    }
+}
